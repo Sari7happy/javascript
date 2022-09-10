697722d052ffd31847e855d716bb8528f87eb969
sari7happy <sari7happy@gmail.com>
2022-09-10 22:58:53 +0900

    if文練習” #

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+let orange = 100;
+let apple = 120;
+if (orange<apple) {
+    alert('みかんの値段がりんごよりやすい');
+} else if (orange==apple) {
+    alert('みかんとりんごが同じ値段');
+} else {
+    alert('みかんの値段がりんごより高い');
+}
 
-let str1='hello'
-let str2='World!!'
-alert(str1+str2)
+
+
+
+
